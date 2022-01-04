6dde51a7d5f4e3fdf810151bb31ba4b33ffd4d5d
Mahmoud Salama <76844592+MahmouddSalama@users.noreply.github.com>
2022-01-05 00:19:54 +0200

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -12,8 +12,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "8640bfe8c5ddeede9a39c927d5e99058",
-"/": "8640bfe8c5ddeede9a39c927d5e99058",
+"index.html": "0dded0fd2d6f365d002a706aa7013e00",
+"/": "0dded0fd2d6f365d002a706aa7013e00",
 "main.dart.js": "fb14d2687247055044fd529324172177",
 "manifest.json": "7e039013f2b79a77fda38a4e9dd8af9f",
 "version.json": "e2f6a4c2cf8b92b9626f501ea1546a35"
